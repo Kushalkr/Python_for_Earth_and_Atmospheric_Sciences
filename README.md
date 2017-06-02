a7ea68a72c76f12bc08a501b225b9dc6e923b113
Kushal Keshavamurthy Raviprakash <kushal@149-160-131-43.dhcp-bl.indiana.edu>
2017-06-02 14:39:51 -0400

    added install script for macOS and Linux

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # Python for Earth and Atmospheric Sciences
+-------------------------------------------
 
-These notebooks were created for a workshop held at Indiana University
-Bloomington and wanted to share it with whoever was interested in learning and
-using python in the Earth and Atmospheric Sciences domain.
+The material here provides a brief overview of the applications of python in the field of Earth and Atmospheric
+Sciences. The objective is to introduce the basics of python to beginners in
+the fields of Earth and Atmospheric sciences. The overview is mainly aimed at
+people in the Earth and Atmospheric Sciences community but it is fairly
+generalized. The basics can be a great resource for beginners to programming and members of other communities as well.
+The list of topics I will be touching up on are as follows:
+
+* Introduction to Python
+* Some introduction to Object-Oriented Programming
+* Arrays and their manipulation
+* Reading and writing to files(Binary, text, netCDF)
+* Visualization of data (line plots, contour plots, map plots, etc...)
+* Timeseries Analysis
+* Parallel Programming in Python
+
+# Links to nbviewer versions
+------------------------------
+
+
+# References
+------------------------------
+Scientific-Computing with Python - Johannsson
+Python for Geosciences - Nikolay Koldunov
+
+
+# Author
+------------------------------
+Kushal Keshavamurthy Raviprakash
---
+++ scripts/miniconda_install.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#set -x
+
+
+# Installs Miniconda and required basic packages for Earth and Atmospheric Science
+#on a macOS machine or Linux Machine
+# 
+# Author: Kushal Keshavamurthy Raviprakash
+#
+# Date Created: May 19 2017
+# Date Modified: 2nd June 2017
+#
+# Usage: bash macOSX_install.sh
+# or
+# chmod +x macOSX_install.sh
+# ./ macOSX_install.sh
+#
+# PS: Please say "YES" when it asks if you want to add Anaconda python to your PATH
+# variable.
+
+printf 'what is your OS? [Linux/MacOS]? ' ; read OS
+
+if [ $OS == 'MacOS' ]; then
+	ifile=Miniconda3-latest-MacOSX-x86_64.sh
+	if ! [ -f $ifile ]; then
+		curl -O https://repo.continuum.io/miniconda/$ifile
+	else
+		check=$(md5 "$ifile" | cut -d' ' -f 4)
+		echo $check
+		if [ $check != 'e46f4555439134cd41ccb914ba283958' ]; then
+			curl -O https://repo.continuum.io/miniconda/$ifile
+		fi
+	fi
+
+	bash $ifile
+
+	source ~/.bash_profile
+elif [ $OS == 'Linux' ]; then
+	ifile=Miniconda3-latest-Linux-x86_64.sh
+	if ! [ -f $ifile ]; then
+		curl -O https://repo.continuum.io/miniconda/$ifile
+	else
+		check=$(md5 "$ifile" | cut -d' ' -f 4)
+		echo $check
+		if [ $check != 'fc6fc37479e3e3fcf3f9ba52cae98991' ]; then
+			curl -O https://repo.continuum.io/miniconda/$ifile
+		fi
+	fi
+
+	bash $ifile
+
+	source ~/.bashrc
+else
+	echo 'Please Enter MacOS or Linux'
+	exit 1
+fi
+
+if [ $? -ne 0 ]; then
+	echo $?
+	exit 1
+fi
+
+conda update -c conda-forge conda
+conda install -c conda-forge numpy scipy numexpr scikit-learn sympy \
+pandas matplotlib basemap netcdf4 hdf5 cython ipython notebook nomkl mpi4py
+
+# Remove the unwanted file
+printf 'do you want to keep the downloaded miniconda installation file? [y/n] '
+read opt
+
+if [ $opt == 'n' ]; then
+	rm $ifile
+fi
+
+exit 0
+
