08eab5487c1cf64597e2ec0924cf51ecf68463b1
Kushal Keshavamurthy Raviprakash <Kushalkr@users.noreply.github.com>
2017-06-16 01:24:26 -0400

    Added 2 exercises

---
+++ scripts/basics_of_python/ex1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Kushal Keshavamurthy Raviprakash
+"""
+
+print("Hello World!")
+
+1 + 1
+2 + 5
+11 - 1
+2 * 5
+2**3
+2/2
+
+print('Hello World!')
+print('Hello Again')
+print('I love printing!')
+
+#Solution:
+
+print(1 + 1)
+print(2 + 5)
+print(11 - 1)
+print(2 * 5)
+print(2**3)
+print(2/2)
+
+# We can print only select lines using comments, like these very lines
+#which the Python interpreter will ignore.
---
+++ scripts/basics_of_python/ex2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: Kushal Keshavamurthy Raviprakash
+"""
+
+a = ["Luke", "Leia", "Obiwan"]
+b = ["Yoda", "Vader", "Death Star"]
+c = ["Storm Trooper", "R2D2"]
+
+# Solution:
+
+d = a + b + c # concatenating the list items.
+
+# Method 1: using del
+print("Method 1: Using del")
+print("Before: ", d)
+del d[4], d[4], d[4]
+# Err what?! yeah that's right. because 4 is the index "Vader"
+# in the list d. but when you delete "Vader", "Death Star"
+# occupies index 4 and same happens to "Storm Trooper".
+print("After: ",d,"\n")
+
+# Method 2: Using the remove(method)
+d = a + b + c     # Resetting list d
+print("Method 2: Using remove()")
+print("Before: ", d)
+d.remove('Vader')
+d.remove('Death Star')
+d.remove('Storm Trooper')
+print("After: ",d,"\n")
+
+# Method 3: Using the pop() method
+d = a + b + c     # Resetting list d
+print("Method 3: Using pop()")
+print("Before: ", d)
+d.pop(4)
+d.pop(4)
+d.pop(4)
+print("After: ",d,"\n")
